6d1edf18151da26bf68796c1a622012170a305b9
yongmin <dragonmin25@gmail.com>
2023-05-02 18:53:42 +0900

    Chore:style 초기화

--- client/app.js
+++ client/app.js
@@ -1,24 +1,13 @@
-function getWeather() {
-  // 날씨 API 요청
-  //   const url = `https://api.openweathermap.org/data/2.5/weather?q=Seoul,kr&appid=4e8805ab0d7fb4c3356c58fa53901f24&units=metric`;
-  const API_KEY = "4e8805ab0d7fb4c3356c58fa53901f24";
-
-  const lat = 37.5815659;
-  const lon = 126.8860337;
-
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
-
-  const cors_url = ``;
-
-  fetch(url)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
-      console.log("온도 = " + data.main.temp);
-      console.log("날씨이미지 이름 = " + data.weather[0].icon);
-    });
+function loadHTML(file) {
+  var rawFile = new XMLHttpRequest();
+  rawFile.open('GET', file, false);
+  rawFile.onreadystatechange = function () {
+    if (rawFile.readyState === 4) {
+      if (rawFile.status === 200 || rawFile.status == 0) {
+        var allText = rawFile.responseText;
+        document.write(allText);
+      }
+    }
+  };
+  rawFile.send(null);
 }
-
-getWeather();
