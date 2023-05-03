ef542f531b14098e88cde7ff53aba12e19a0e959
박지윤 <jyun1937@naver.com>
2023-05-03 09:31:18 +0900

    Style: 전체 레이아웃 스타일 구현

--- client/app.js
+++ client/app.js
@@ -1,13 +1,20 @@
-function loadHTML(file) {
-  var rawFile = new XMLHttpRequest();
+/**
+ * @description html 파일 불러와서 로드하는 함수
+ * @param file html 파일 경로
+ *
+ */
+
+const htmlRoot = ['src/recommend/index.html'];
+const loadHTML = file => {
+  const rawFile = new XMLHttpRequest();
   rawFile.open('GET', file, false);
   rawFile.onreadystatechange = function () {
     if (rawFile.readyState === 4) {
       if (rawFile.status === 200 || rawFile.status == 0) {
-        var allText = rawFile.responseText;
+        const allText = rawFile.responseText;
         document.write(allText);
       }
     }
   };
   rawFile.send(null);
-}
+};
