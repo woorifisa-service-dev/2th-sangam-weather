4619cc4c4eba7f1d9a4271126cbdfee2c7bd18d9
jiny798 <jyoung7988@naver.com>
2023-05-03 14:56:52 +0900

    Fix: 날씨 API 구현 코드 수정

--- client/src/wheather/main.js
+++ client/src/wheather/main.js
@@ -4,27 +4,28 @@ async function getWeather() {
 
   const cors_url = ``;
 
-  await fetch(url)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (result) {
-      console.log('날씨 요청 받아옴');
-      console.log(result);
+  try {
+    const result = await (await fetch(url)).json();
 
-      // result.humidity : 습도
-      // result.speed : 풍속
-      document.getElementById('weather-img').src = './src/img/weather/' + result.icon + '.png';
-      let temperatureTextBox = document.getElementById('temperature-box');
-      let humidityTextBox = document.getElementById('humidityText-box');
-      let speedTextBox = document.getElementById('speedText-box');
-      let rainTextBox = document.getElementById('rainText-box');
-
-      temperatureTextBox.innerText = result.temp;
-      humidityTextBox.innerText = result.humidity;
-      speedTextBox.innerText = result.speed;
-      rainTextBox.innerText = result.rain;
-    });
+    // result.speed : 풍속
+    return result;
+  } catch (error) {
+    // 날씨 API 에러 발생시 처리
+    alert('서버에 문제가 생겼습니다.');
+  }
 }
 
-getWeather();
+var result;
+(async () => {
+  result = await getWeather();
+  document.getElementById('weather-img').src = './src/img/weather/' + result.icon + '.png';
+  let temperatureTextBox = document.getElementById('temperature-box');
+  let humidityTextBox = document.getElementById('humidityText-box');
+  let speedTextBox = document.getElementById('speedText-box');
+  let rainTextBox = document.getElementById('rainText-box');
+
+  temperatureTextBox.innerText = result.temp;
+  humidityTextBox.innerText = result.humidity;
+  speedTextBox.innerText = result.speed;
+  rainTextBox.innerText = result.rain;
+})();
--- server/openWeather.js
+++ server/openWeather.js
@@ -3,7 +3,6 @@ dotenv.config({ path: '../.env' }); //process.env.API_KEY
 
 //강수량 가져오는 함수 - 없으면 0 반환
 function getRain(data) {
-  // var key = eval('1h');
   return data.rain ? data.rain['1h'] : 0;
 }
 
@@ -11,44 +10,29 @@ const weather = app => {
   app.get('/weather', async (req, res) => {
     const lat = 37.5815659;
     const lon = 126.8860337;
-
-    //https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid={API key}
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.API_KEY}&units=metric`;
-
-    // const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${process.env.API_KEY}&units=metric`;
-
     let result = {};
 
-    await fetch(url) // axios라이브러리 사용 시 json 처리 추상화
-      .then(response => response.json())
-      .then(data => {
-        console.log('api서버에서 받아온 데이터');
-        console.log(data);
+    try {
+      await fetch(url) // axios라이브러리 사용 시 json 처리 추상화
+        .then(response => response.json())
+        .then(data => {
+          result.icon = data.weather[0].icon;
+          result.description = data.weather[0].description;
 
-        result.icon = data.weather[0].icon;
-        result.description = data.weather[0].description;
+          result.temp = data.main.temp;
+          result.humidity = data.main.humidity;
+          result.speed = data.wind.speed;
+          result.rain = getRain(data);
 
-        result.temp = data.main.temp;
-        result.humidity = data.main.humidity;
-        result.speed = data.wind.speed;
-
-        result.rain = getRain(data);
-
-        // console.log('날씨 api 정보' + result.temp);
-        res.send(result);
-      })
-      .catch(error => console.error(error));
-
-    // res.send(result);
+          res.send(result);
+        });
+    } catch (err) {
+      //에러처리
+    }
   });
 
   return app;
 };
 
 export default weather;
-
-// app.get('/restaurantInfo', (req, res) => {
-//     if (req.method === 'GET') {
-//       res.status(200).json(restaurantList);
-//     }
-//   });
